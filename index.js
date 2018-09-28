c2683637554c616ed0f94e712a5e8ceabd5493a6
deekshasharma <deeksha.sharma25@gmail.com>
2018-09-28 15:00:54 -0400

    Added a console log and modified submodule

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log('Hold on! We are trying git submodule here!');
-console.log('Added: git config push.recurseSubmodules check');
-
 console.log('Only making changes to git-submodule-example project');
+console.log('Added -> git config push.recurseSubmodules check without pushing submodule first ');
