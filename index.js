e78984efedb1f457622ac3bb7aa674e5afbd7ee4
deekshasharma <deeksha.sharma25@gmail.com>
2018-09-28 15:08:57 -0400

    Forgot to commit and push submodule-doing it now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('Hold on! We are trying git submodule here!');
 console.log('Only making changes to git-submodule-example project');
 console.log('Added -> git config push.recurseSubmodules check without pushing submodule first ');
+console.log('Forgot to commit and push submodule doing it now');
+
