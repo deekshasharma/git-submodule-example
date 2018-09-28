632b33cbb53e5f2261befd36a3fe3bb9b07d67e2
deekshasharma <deeksha.sharma25@gmail.com>
2018-09-28 17:25:09 -0400

    Added console log and action COMMIT2_MAIN_AND_SUBMODULE_CHANGES

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ console.log('Only making changes to git-submodule-example project');
 console.log('Added -> git config push.recurseSubmodules check without pushing submodule first ');
 console.log('Forgot to commit and push submodule doing it now');
 console.log('adding changes to main project and submodules both');
+console.log('Second time adding changes to main project and submodules both');
 
